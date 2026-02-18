0f3d1b2216cfc1899b2b9d3493fdf319b0d132af
Akeanant Poomdeesittinon <aekanan2004@gmail.com>
2026-02-18 17:05:00 +0700

    feat: auth func api

---
+++ lib/auth/auth.ts
@@ -0,0 +1,50 @@
+import api from "../api/api";
+import { supabase } from "../supabaseClient";
+
+export const signInWithGoogle = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+            redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/callback`,
+        },
+    });
+
+    if (error) throw error;
+    return data;
+}
+
+export const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    window.location.reload();
+}
+
+export const getSession = async () => {
+    const { data: { session } } = await supabase.auth.getSession();
+    return session;
+}
+
+export const getUser = async () => {
+    const { data: { user } } = await supabase.auth.getUser();
+
+    return {
+        id: user?.id,
+        email: user?.email,
+        avatar: user?.user_metadata.avatar_url,
+        name: user?.user_metadata.full_name,
+    };
+}
+
+export const createUser = async () => {
+    const { data: { user } } = await supabase.auth.getUser();
+
+    const res = await api.url("/api/auth/login").post({
+        id: user?.id,
+        email: user?.email,
+        avatar: user?.user_metadata.avatar_url,
+        name: user?.user_metadata.full_name,
+    }).json();
+    
+    if(!res) return
+    return res;
+}
